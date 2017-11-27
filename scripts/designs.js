7f159e231faa296bfcdfb4bcc122fdc81c091cda
Harika <harika317@yahoo.com>
2017-11-27 19:08:26 +0000

    Initial commit - Pixel art maker game

--- scripts/designs.js
+++ scripts/designs.js
@@ -9,22 +9,25 @@ var lastClicked, inputHeight, inputWidth;
 inputHeight = $("#input_height").val();
 inputWidth = $("#input_width").val();
 
-$("#btnSubmit").click(function(inputHeight,inputWidth){
-    var returnedGrid = ownGrid(inputHeight,inputWidth);
+$("#btnSubmit").click(function (e) {
+    $('.grid').remove();
+    e.preventDefault();
+    inputHeight = $("#input_height").val();
+    inputWidth = $("#input_width").val();
+    var returnedGrid = ownGrid(inputHeight, inputWidth);
     document.body.appendChild(returnedGrid);
-    
+
 
 });
 
-function ownGrid(rows,cols){
+function ownGrid(rows, cols) {
     var grid = document.createElement('table');
     grid.className = 'grid';
-    for (var r=0;r<rows;++r){
+    for (var r = 0; r < rows; ++r) {
         var tr = grid.appendChild(document.createElement('tr'));
-        for (var c=0;c<cols;++c){
+        for (var c = 0; c < cols; ++c) {
             var cell = tr.appendChild(document.createElement('td'));
         }
     }
     return grid;
 }
-
