a62c3748b1e901506ed7ec6be101fe93069d98f7
Harika <harika317@yahoo.com>
2017-11-27 15:06:36 +0000

    Initial code

---
+++ designs.js
@@ -0,0 +1,30 @@
+// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+
+
+var lastClicked, inputHeight, inputWidth;
+
+inputHeight = $("#input_height").val();
+inputWidth = $("#input_width").val();
+
+$("#btnSubmit").click(function(inputHeight,inputWidth){
+    var returnedGrid = ownGrid(inputHeight,inputWidth);
+    document.body.appendChild(returnedGrid);
+    
+
+});
+
+function ownGrid(rows,cols){
+    var grid = document.createElement('table');
+    grid.className = 'grid';
+    for (var r=0;r<rows;++r){
+        var tr = grid.appendChild(document.createElement('tr'));
+        for (var c=0;c<cols;++c){
+            var cell = tr.appendChild(document.createElement('td'));
+        }
+    }
+    return grid;
+}
+
